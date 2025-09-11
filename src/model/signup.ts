9d38d08292c8727b91f2c8b528014d6517044467
Abhi <jagodanaabhay@gmail.com>
2025-09-11 10:05:15 +0530

    netflix

--- src/model/signup.ts
+++ src/model/signup.ts
@@ -5,26 +5,22 @@ const signupSchema = new Schema({
     type: String,
     required: true,
     trim: true,
-    match: /^[a-zA-Z\s]+$/,
   },
   email: {
     type: String,
     required: true,
     unique: true,
     trim: true,
-    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
   },
   phone: {
     type: String,
     required: true,
     trim: true,
-    match: /^\+?[1-9]\d{1,14}$/,
   },
   password: {
     type: String,
     required: true,
     trim: true,
-    minLength: 6,
   },
   createdAt: {
     type: Date,
